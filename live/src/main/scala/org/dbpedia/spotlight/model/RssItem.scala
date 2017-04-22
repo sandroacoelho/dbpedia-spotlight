8fa5a899f5ab2dfebdf666ad14724840299b9e24
sandro.coelho <sandroacoelho@gmail.com>
2017-04-21 22:31:25 -0300

    Codebase cleanup

--- live/src/main/scala/org/dbpedia/spotlight/model/RssItem.scala
+++
@@ -1,9 +0,0 @@
-package org.dbpedia.spotlight.model
-
-import java.util.Date
-import java.net.URL
-
-/**
- * RSS item wrapper class.
- */
-class RssItem(val title: Text, val date: Date, val description: Text, val link: URL)
